90f6aa064beb4c65a0b8e620f17c9602f5c608eb
Jaecheol Park <nature9th@gmail.com>
2024-01-19 20:24:22 +0900

    refactor: control ExpansionTile

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 import 'ui/expansion_tile_controller.dart';
-import 'ui/image_alignment.dart';
 
 void main() {
   runApp(const MyApp());
--- lib/ui/expansion_tile_controller.dart
+++ lib/ui/expansion_tile_controller.dart
@@ -19,73 +19,75 @@ class _ExpansionTileSampleState extends State<ExpansionTileSample> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: StatefulBuilder(builder: (
-        BuildContext context,
-        StateSetter setInternalState,
-      ) {
-        return ListView(children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 8),
-            child: Divider(
-              height: 2,
-              color: Colors.grey,
-            ),
-          ),
-          ExpansionTile(
-              controller: tileControllers[0],
-              onExpansionChanged: (isExpanded) {
-                if (tileControllers[1].isExpanded) {
-                  tileControllers[1].collapse();
-                }
-                if (isExpanded) {
-                  tileControllers[0].expand();
-                } else {
-                  tileControllers[0].collapse();
-                }
-              },
-              initiallyExpanded: true,
-              maintainState: false,
-              shape: Border(),
-              title: Text(
-                'Описание на продукта',
-                style: TextStyle(fontWeight: FontWeight.w500),
+      body: StatefulBuilder(
+        builder: (_, __) {
+          return ListView(
+            children: [
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 8),
+                child: Divider(
+                  height: 2,
+                  color: Colors.grey,
+                ),
               ),
-              children: [
-                // htmlView
-                Text('htmlView'),
-              ]),
-          ExpansionTile(
-              controller: tileControllers[1],
-              onExpansionChanged: (isExpanded) {
-                if (tileControllers[0].isExpanded) {
-                  tileControllers[0].collapse();
-                }
-                if (isExpanded) {
-                  tileControllers[1].expand();
-                } else {
-                  tileControllers[1].collapse();
-                }
-              },
-              shape: Border(),
-              title: Text(
-                'Описание на продукта 2',
-                style: TextStyle(fontWeight: FontWeight.w500),
+              ExpansionTile(
+                controller: tileControllers[0],
+                onExpansionChanged: (isExpanded) {
+                  if (tileControllers[1].isExpanded) {
+                    tileControllers[1].collapse();
+                  }
+                  if (isExpanded) {
+                    tileControllers[0].expand();
+                  } else {
+                    tileControllers[0].collapse();
+                  }
+                },
+                initiallyExpanded: true,
+                shape: const Border(),
+                title: const Text(
+                  'Описание на продукта',
+                  style: TextStyle(fontWeight: FontWeight.w500),
+                ),
+                children: const [
+                  // htmlView
+                  Text('htmlView'),
+                ],
               ),
-              children: [
-                Text('12'),
-                Text('12'),
-                Text('12'),
-                Text('12'),
-              ]),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 8),
-            child: Divider(
-              height: 2,
-              color: Colors.grey,
-            ),
-          ),
-        ]);
-      }),
+              ExpansionTile(
+                controller: tileControllers[1],
+                onExpansionChanged: (isExpanded) {
+                  if (tileControllers[0].isExpanded) {
+                    tileControllers[0].collapse();
+                  }
+                  if (isExpanded) {
+                    tileControllers[1].expand();
+                  } else {
+                    tileControllers[1].collapse();
+                  }
+                },
+                shape: const Border(),
+                title: const Text(
+                  'Описание на продукта 2',
+                  style: TextStyle(fontWeight: FontWeight.w500),
+                ),
+                children: const [
+                  Text('12'),
+                  Text('12'),
+                  Text('12'),
+                  Text('12'),
+                ],
+              ),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 8),
+                child: Divider(
+                  height: 2,
+                  color: Colors.grey,
+                ),
+              ),
+            ],
+          );
+        },
+      ),
     );
   }
 }
